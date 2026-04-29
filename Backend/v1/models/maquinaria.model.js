185aa66a5ea37167eca1ecc8e763a58b8f6bc401
camiborrelli <camilaborrelligobertti@gmail.com>
2026-04-29 20:51:27 -0300

    Cantidad de reparaciones

--- Backend/v1/models/maquinaria.model.js
+++ Backend/v1/models/maquinaria.model.js
@@ -21,6 +21,10 @@ export const MaquinariaSchema = new Schema({
     ref: "Obra",
     default: null,
   },
+  cantReparaciones: {
+    type: Number,
+    default: 0,
+  },
 });
 
 export default mongoose.model("Maquinaria", MaquinariaSchema);
--- Backend/v1/services/maquinaria.services.js
+++ Backend/v1/services/maquinaria.services.js
@@ -108,6 +108,7 @@ export const asignarMaquinariaMantenimientoServices = async (id, obraId) => {
     throw new Error("Maquinaria no encontrada");
   }
   maquinaria.estado = "En mantenimiento";
+  maquinaria.cantReparaciones = (maquinaria.cantReparaciones || 0) + 1;
   await maquinaria.save();
   return maquinaria;
 };
