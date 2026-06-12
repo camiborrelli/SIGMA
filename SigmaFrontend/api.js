6c22ef333c0326f11733a365bed6def115b40b2d
camiborrelli <camilaborrelligobertti@gmail.com>
2026-06-11 21:37:37 -0300

    api.js con axios

--- SigmaFrontend/api.js
+++ SigmaFrontend/api.js
@@ -1,1 +1,9 @@
-API_URL= sigma-backend-jp8vvxiej-camilas-projects-2b00654e.vercel.app
+import axios from "axios";
+
+const API_URL = "sigma-backend-jp8vvxiej-camilas-projects-2b00654e.vercel.app";
+export default axios.create({
+  baseURL: API_URL,
+  headers: { "Content-Type": "application/json" },
+});
+
+export { API_URL };
