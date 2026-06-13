cdfd0334d80a3f86ef106568d49c48f7462a9b5d
camiborrelli <camilaborrelligobertti@gmail.com>
2026-06-13 15:14:05 -0300

    Add render for deploy

--- SigmaFrontend/api.js
+++ SigmaFrontend/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_URL =
-  "https://sigma-backend-jp8vvxiej-camilas-projects-2b00654e.vercel.app";
+const API_URL = "https://sigma-83pz.onrender.com";
 export default axios.create({
   baseURL: API_URL,
   headers: { "Content-Type": "application/json" },
